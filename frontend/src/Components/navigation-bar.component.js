f9ebd4652b8f9f284eb71bb68f531cf89b6e8ba0
Daniel Kimball <danielkimball@daniels-mbp.home>
2020-05-19 14:00:10 -0400

    Navbar

--- frontend/src/Components/navigation-bar.component.js
+++ frontend/src/Components/navigation-bar.component.js
@@ -9,10 +9,13 @@ export default class NavigationBar extends Component {
       <nav id="NavigationBar" class="navigation-bar">
         <img src={logo} alt="logo" className="logo" />
         <h1> Learning Curve </h1>
-        <Link to="/">
+        <Link to="/" className="navLinkFont">
           <h3> Home </h3>
         </Link>
-        <Link to="/entry">
+        <Link to="/sessions" className="navLinkFont">
+          <h3> Sessions </h3>
+        </Link>
+        <Link to="/entry" className="navLinkFont">
           <h3> SignUp </h3>
         </Link>
       </nav>
