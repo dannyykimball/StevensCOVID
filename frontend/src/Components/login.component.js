ee9b413ef3077df0ff72e0a4e5230dfcca7895cc
Daniel Kimball <danielkimball@daniels-mbp.home>
2020-05-21 23:03:24 -0400

    Home

--- frontend/src/Components/login.component.js
+++ frontend/src/Components/login.component.js
@@ -1,42 +1,41 @@
 import React, { useState, useContext } from "react";
 import axios from "axios";
-import UserContext from '../userContext';
+import UserContext from "../userContext";
 
 export default function Login(props) {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const userContext = useContext(UserContext)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const userContext = useContext(UserContext);
 
   function onChangeEmail(e) {
-    setEmail(e.target.value)
+    setEmail(e.target.value);
   }
 
   function onChangePassword(e) {
-    setPassword(e.target.value)
+    setPassword(e.target.value);
   }
 
   function onSubmit(e) {
     e.preventDefault();
     var user = {
       email: email,
-      password: password
-    }
-    console.log(user)
-    console.log(userContext)
+      password: password,
+    };
+    console.log(user);
+    console.log(userContext);
     axios
       .post("http://" + window.location.hostname + ":5000/User/Login", user)
       .then((res) => {
-        console.log(res.data)
-
-
-      }).catch((err) => { console.log(err) });
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
-
-
 
   return (
     <div className="signupbox orange">
-      <h3 className="kinder">Login</h3>
+      <h3 className="learners center">Login</h3>
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Email: </label>
@@ -53,17 +52,21 @@ export default function Login(props) {
           <label>Password: </label>
           <input
             type="text"
-            className="form-control"
+            className="form-control password"
             value={password}
             onChange={onChangePassword}
           />
         </div>
 
         <div className="form-group">
-          <input type="submit" value="Log In" className="btn" onClick={onSubmit} />
+          <input
+            type="submit"
+            value="Log In"
+            className="btn"
+            onClick={onSubmit}
+          />
         </div>
       </form>
     </div>
   );
-
 }
